4da47b9268aca1a38898f2cddf5f76f8f21e895e
Marcelo Lucas <marcelo.lucas.eng@gmail.com>
2023-05-01 02:07:44 -0300

    commit inicial 2023-05-01

---
+++ _javascript/funcoes.js
@@ -0,0 +1,8 @@
+// ----------------------------------- FUNÇÃO PARA MUDAR A FOTO -----------------------------------
+	<script>
+		function mudaFoto (foto)
+		{
+			document.getElementById("icone").src = foto;
+			// Função de mudar o icone da foto
+		}
+	</script>
